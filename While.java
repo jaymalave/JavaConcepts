e2beb032d8c26eb69025db7f294cabbdde943029
jaymalave <jaym.github@gmail.com>
2021-06-30 15:23:50 +0530

    added while

--- While.java
+++ While.java
@@ -1,3 +1,9 @@
 public class While {
-      
+      public static void main(String args[]){
+          int i = 0;
+          while(i < 10){
+              System.out.println(i*3);
+              i++;
+          }
+      }
 }
