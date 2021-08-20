ec2f20f5407cfde297ee83460a547d6afea0fe36
jaymalave <jaym.github@gmail.com>
2021-08-20 15:08:07 +0530

    added a separate class for gcd

--- RecGCD.java
+++ RecGCD.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class RecGCD {
 
-   public static int findGCD(int n1, int n2){
-       if(n2 != 0){
-           return findGCD(n2, n1%n2);
-       }else{
-           return n1;
-       }
-   }
-
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers");
         int x = sc.nextInt();
         int y = sc.nextInt();
-        int result = findGCD(x, y);
+        // GCD g1 = new GCD();
+        int result = GCD.findGCD(x, y);
         System.out.println(result);
         sc.close();
+    }
+}
+
+class GCD{
+     static int findGCD(int n1, int n2){
+        if(n2 != 0){
+            return findGCD(n2, n1%n2);
+        }else{
+            return n1;
+        }
     }
 }
 
