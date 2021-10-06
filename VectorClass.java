1fd6e937fa553fe71f110d5e183838b204598778
jaymalave <jaym.github@gmail.com>
2021-10-07 01:50:05 +0530

    vector funcs

--- VectorClass.java
+++ VectorClass.java
@@ -18,12 +18,34 @@ public class VectorClass {
              Vector<Dogs> Kennel = new Vector<Dogs>();
              Kennel.add(new Dogs("Tom", 1, false));
              Kennel.add(new Dogs("Bravo", 2, true));
+             Kennel.add(new Dogs("Corey", 3, false));
+             Kennel.add(new Dogs("Mat", 4, true));
+             String dog_to_search = "Corey";
+             String dog_to_remove = "Mat";
+             int new_dog_id = 5;
+             String new_dog_name = "Sam";
+             boolean new_dog_isStray = true;
+             
+             Kennel.addElement(new Dogs(new_dog_name, new_dog_id, new_dog_isStray));
+             
+             Kennel.add(1, new Dogs("Chris", 6, false));
 
              for(int i = 0; i < Kennel.size(); i++) {
+            	 Dogs check = Kennel.get(i);
+            	 if(dog_to_remove.equals(check.d_name)) {
+            		 Kennel.remove(i);
+            	 } 	 
+            	 //array index out of range error after this
+            	 //create a separate function for this
             	 String req_name = Kennel.get(i).d_name;
             	 int req_id = Kennel.get(i).d_id;
             	 boolean req_isStray = Kennel.get(i).d_isStray;
             	 System.out.println(req_name + req_id + req_isStray);
+            	 
+            	 if(dog_to_search.equals(check.d_name)){
+            		 System.out.println("Corey is there");
+            	 }
+            	 
              }
     }
 }
--- Wrapper.java
+++ Wrapper.java
@@ -1,4 +1,15 @@
 
-public class Wrapper {
+import java.util.*;
 
+class Wrapper {
+    public static void main(String[] args) {
+        int n = 5;
+        Integer i = new Integer(n);
+        System.out.println(i);
+        
+       ArrayList<Integer> values = new ArrayList<Integer>();
+       values.add(1);
+       values.add(2);
+       values.add(5);
+    }
 }
